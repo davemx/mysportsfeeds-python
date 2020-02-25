0c3781445fefbb2ec7dad2d9247a2625efe5564f
Dave Mc <dave@davemx.com>
2020-02-24 23:25:46 -0700

    Bug: Encoding error.

--- ohmysportsfeedspy/stores.py
+++ ohmysportsfeedspy/stores.py
@@ -229,11 +229,7 @@ def _write_data(data: Any, data_format: str, output_stream: IO) -> None:
 
 def _store_temp_file(data: Any, data_format: str) -> NamedTemporaryFile:
     """ Writes the data to a temporary file and returns the file. """
-    if isinstance(data, str):
-        temp_file: NamedTemporaryFile = NamedTemporaryFile(mode="w+", suffix=f".{data_format}")
-    else:
-        temp_file: NamedTemporaryFile = NamedTemporaryFile(mode="w+b", suffix=f".{data_format}")
-
+    temp_file: NamedTemporaryFile = NamedTemporaryFile(mode="w+b", suffix=f".{data_format}")
     _write_data(data, data_format, temp_file)
     temp_file.seek(0)
     return temp_file
